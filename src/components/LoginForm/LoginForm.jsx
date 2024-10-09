b96fbe1a52243afc675a71e6a335260b57860f70
Shwetas Dhake <shwetasdhake16@gmail.com>
2024-10-10 00:18:34 +0530

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,31 +1,54 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React from 'react';
 import { GithubLogin, login } from '@/lib/action';
-import { FaGithub } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa';
 import { useFormState } from 'react-dom';
 import Link from 'next/link';
 
 const LoginForm = () => {
-
-    const [state, formAction] = useFormState(login, undefined);
+    const [state, formAction] = useFormState(login);
 
     return (
-        <section id='login-form'>
-            <form className='form' action={formAction}>
-                <input required type="text" name="username" placeholder='User Name' />
-                <input required type="password" name="password" placeholder='Password' />
-                <button> Login with Credentials </button>
+        <section id="login-form">
+            <form className="form" action={formAction}>
+                <input
+                    required
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                />
+                <input
+                    required
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                />
+                <button type="submit">Login with Credentials</button>
             </form>
             <form action={GithubLogin}>
-                <button className='login-github'> Login with github <FaGithub /> </button>
+                <button className="login-github" type="submit">
+                    Login with GitHub <FaGithub />
+                </button>
             </form>
-            {state?.error && <div style={{ marginBottom: '13px', marginTop: '13px' }} className='error'> {state.error}  </div>}
-            <Link href='/register'>
-                Don't have an account ? <b>Register</b>
+            {state?.error && (
+                <div
+                    style={{
+                        marginBottom: '13px',
+                        marginTop: '13px',
+                    }}
+                    className="error"
+                >
+                    {state.error}
+                </div>
+            )}
+            <Link href="/register">
+                Don't have an account? <b>Register</b>
             </Link>
         </section>
-    )
-}
+    );
+};
 
-export default LoginForm
+export default LoginForm;
+
+
